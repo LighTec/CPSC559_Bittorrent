79730c1be1ecda0fe0e7751cb37fe0780e2ab778
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-03-26 14:32:33 -0600

    Test for connecting to global tracker and receiving heartbeats

--- Controller/ClientMain.java
+++ Controller/ClientMain.java
@@ -1,14 +1,15 @@
 package Controller;
 
-import Network.Client.UDPClient;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class ClientMain {
-    public static void main(String[] args) throws UnknownHostException  //client driver
-    {
+
+    public static void main(String[] args) {
+        /*
         InetAddress inetAddress = InetAddress.getLocalHost();
         String url = inetAddress.getHostAddress();
         String port = "6006";
@@ -20,5 +21,21 @@ public class ClientMain {
         //data.add(connection2); // uncomment to add more peers
         UDPClient client = new UDPClient(data); //init and start stuff above
         client.start();
+        */
+
+        try {
+            Socket client = new Socket("localhost", 1962);
+
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            out.println("connect");
+            System.out.println("<< Nodes: " + in.readLine());
+
+            MessageThread mt = new MessageThread();
+            mt.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
