84080cad6a099bd3f0e1b452cf7fa5d524ef2114
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-04-01 18:35:47 -0600

    Created NodeList class

--- Controller/ClientMain.java
+++ Controller/ClientMain.java
@@ -1,15 +1,10 @@
 package Controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Arrays;
+import Network.NodeList;
 
 public class ClientMain {
 
-    public static String[] nodes;
+    static String[] nodes;
 
     public static void main(String[] args) {
         /*
@@ -26,25 +21,10 @@ public class ClientMain {
         client.start();
         */
 
-        try {
-            Socket client = new Socket("localhost", 1962);
+        NodeList nl = new NodeList();
+        nodes = nl.getNodes();
 
-            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
-            out.println("connect");
-            StringBuilder nodesString = new StringBuilder();
-            String temp;
-            while ((temp = in.readLine()) != null) {
-                nodesString.append(temp);
-            }
-            nodes = nodesString.toString().split(",");
-            System.out.println(Arrays.toString(nodes));
-
-            MessageThread mt = new MessageThread();
-            mt.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        MessageThread mt = new MessageThread();
+        mt.start();
     }
 }
